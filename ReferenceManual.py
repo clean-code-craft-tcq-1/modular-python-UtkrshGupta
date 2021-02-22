04a6f02c79143200e5771aeafdb2822478ab00da
Utkarsh Gupta <ututkarshgupta7@gmail.com>
2021-02-22 15:45:02 +0530

    Commit2
    Tabulate package not supported tried with PrettyTable

--- ReferenceManual.py
+++ ReferenceManual.py
@@ -2,15 +2,15 @@ from ColorPair import get_total_major_colors as major_len
 from ColorPair import get_total_minor_colors as minor_len 
 import ColorPairGenerator as cpg
 
-from tabulate import tabulate 
+from prettytable import PrettyTable 
 
 
 def createReferenceData():
     pair_number_start_index = 1
     pair_number_end_index = major_len()*minor_len() + 1     
-    reference_data = []
+    reference_data = PrettyTable(['Pair Number', 'Color Code Pairs'])
     for pair_number in range(pair_number_start_index, pair_number_end_index):
         major_color, minor_color = cpg.get_color_from_pair_number(pair_number)
         formatted_colorpair = cpg.color_pair_to_string(major_color, minor_color)
-        reference_data.append([pair_number,formatted_colorpair])
-    return tabulate(reference_data, headers=['Pair Number', 'Color Pair'], tablefmt='github')
+        reference_data.add_row([pair_number,formatted_colorpair])
+    return reference_data
